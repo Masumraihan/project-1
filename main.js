311a0ee659180deb0c0f99e55e02b6ec10cf32ec
Masumraihan <mdmasumraihan1@gamil.com>
2024-04-19 16:20:14 +0600

    header created

--- main.js
+++ main.js
@@ -1,24 +1,42 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+import viteLogo from "/vite.svg";
+import EmblaCarousel from "embla-carousel";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs text-red-500">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const emblaNode = document.querySelector(".embla");
+const options = { loop: false, autoplay: true, speed: 10 };
+const emblaApi = EmblaCarousel(emblaNode, options);
 
-setupCounter(document.querySelector('#counter'))
+//console.log(emblaApi.slideNodes()); // Access API
+
+// NAVBAR CATEGORIES
+const categories = [
+  "All",
+  "Sci-fi",
+  "Drama",
+  "Action",
+  "Horror",
+  "All",
+  "Sci-fi",
+  "Drama",
+  "Action",
+  "Horror",
+  "All",
+  "Sci-fi",
+  "Drama",
+  "Action",
+  "Horror",
+  "All",
+  "Sci-fi",
+  "Drama",
+  "Action",
+  "Horror",
+];
+
+const emblaContainer = document.getElementById("emblaContainer");
+categories.forEach((category) => {
+  const div = document.createElement("div");
+  div.classList.add("embla__slide");
+  div.innerHTML = `<a href="#" class="text-center text-sm font-semibold">${category}</a>`;
+  emblaContainer.appendChild(div);
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html","./main.js", "./src/*.{html,js,css}"],
+  content: ["./index.html", "./main.js", "./src/*.{html,js,css}"],
+
   theme: {
     extend: {},
+    container: {
+      center: true,
+    },
   },
-  plugins: [],
+  plugins: [require("daisyui"), require("@tailwindcss/forms")],
   mode: "jit",
+
+  daisyui: {
+    themes: [
+      "light",
+      "dark",
+      "cupcake",
+      {
+        customLight: {
+          primary: "#1ABA1A",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+          "base-200": "#F5F5F5",
+          "base-300": "#ffffff",
+        },
+      },
+    ],
+  },
 };
